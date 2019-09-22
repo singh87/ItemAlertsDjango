2bf033db2f39809dec78a797a40fd2e44f74b420
singh87 <singh87@uwm.edu>
2019-09-22 14:14:58 -0500

    Added search to frontend

--- mercari/frontend/src/actions/listings.js
+++ mercari/frontend/src/actions/listings.js
@@ -12,3 +12,6 @@ export const getListings = () => dispatch => {
             })
         }).catch(err => console.log(err));
 }
+
+// NEW SEARCH
+// export const 
--- mercari/frontend/src/components/layout/Header.js
+++ mercari/frontend/src/components/layout/Header.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
-import { PageHeader } from 'antd';
+import { Typography, Input, Col, Row } from 'antd';
+import Title from "antd/lib/typography/Title";
+
+const { Search } = Input;
+
 
 export class Header extends Component {
   render() {
     return (
       <div>
-        <PageHeader title="Mercari Alerts" />
+        <header id="header" style={{padding:"10px 15px"}} >
+          <Row>
+            <Col span={4}>
+              <Typography>
+                <Title level={2}>Mercari Alerts</Title>
+              </Typography>
+              
+            </Col>
+            <Col span={16}>
+              <Search
+                placeholder="Search"
+                enterButton="Search"
+                size="large"
+                allowClear="true"
+                onSearch={value => console.log(value)}
+              />
+            </Col>  
+          </Row>
+        </header>
       </div>
     );
   }
